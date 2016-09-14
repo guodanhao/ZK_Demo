7017340f3a1f4e99688ae9c27f3376a0cc6580f0
muxi <muxi@2dfire.com>
2016-09-14 13:38:12 +0800

    完成Master

--- zk/src/main/java/watcher/Master.java
+++ zk/src/main/java/watcher/Master.java
@@ -2,13 +2,38 @@ package watcher;
 
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
 
 /**
  * Created by muxi on 2016/9/14.
  */
 public class Master implements Watcher {
 
-    public void process(WatchedEvent watchedEvent) {
+    ZooKeeper zk;
+    String hostPort;
 
+    public void process(WatchedEvent watchedEvent) {
+        // 打印Event
+        System.out.print(watchedEvent);
+    }
+
+    Master(String hostPort){
+        this.hostPort = hostPort;
+    }
+
+    // 需要多一个throw，不知道是不是版本问题
+    void startZK() throws IOException {
+        zk = new ZooKeeper(hostPort, 15000, this);
+    }
+
+    public static void main(String[] args)
+        throws Exception {
+
+        Master master = new Master(args[0]);
+        master.startZK();
+//
+        Thread.sleep(6000);
     }
 }
