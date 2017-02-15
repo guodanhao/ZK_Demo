1f8056cfd551aa5c6db3082114f79776bcd68281
muxi <muxi@2dfire.com>
2017-02-15 14:24:15 +0800

    first step

---
+++ src/main/java/service/FirstHandler.java
@@ -0,0 +1,13 @@
+package service;
+
+import service.Imp.HandlerImp;
+
+/**
+ * Created by muxi on 2017/2/15.
+ */
+public class FirstHandler implements HandlerImp {
+
+    public void process() {
+
+    }
+}
---
+++ src/main/java/service/Imp/HandlerImp.java
@@ -0,0 +1,13 @@
+package service.Imp;
+
+/**
+ * Created by muxi on 2017/2/15.
+ */
+public interface HandlerImp {
+
+    /**
+     * 触发方法
+     */
+    void process();
+
+}
